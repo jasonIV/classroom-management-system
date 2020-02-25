743dd10d2e3e2076b980557ab4348f3eac00e1dd
jasonIV <thuhtetaung.d2@gmail.com>
2020-02-26 00:09:48 +0630

    added switch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { Route,Router } from "react-router-dom";
+import { Route, BrowserRouter, Switch} from "react-router-dom";
 import Login from './containers/Login';
 import Years from './containers/Years'
 import StudentInfo from './containers/StudentInfo';
 import Attendance from './containers/Attendence';
 import LabReport from './containers/LabReport';
 import Assignment from'./containers/Assignments';
-import history from './history';
+//import history from './history';
 
 class App extends Component{
     render(){
         return(
-            <Router history={history}>
-                <div>
+            <BrowserRouter>
+                <Switch>
                     <Route exact path="/login" component={Login}/>
                     <Route exact path="/" component={Login}/>
                     <Route exact path="/years" component={Years}/>
@@ -20,8 +20,8 @@ class App extends Component{
                     <Route exact path="/attendace" component={Attendance}/>
                     <Route exact path="/labreport" component={LabReport}/>
                     <Route exact path="/assignment" component={Assignment}/>
-                </div>
-            </Router>
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
